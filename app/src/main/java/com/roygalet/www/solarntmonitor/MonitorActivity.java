5577572b42c17b75bbdfc33a0dd3056d6c013a35
Roy Galet <Roy Galet>
2016-10-16 11:53:51 +0930

    Initial commit

---
+++ app/src/main/java/com/roygalet/www/solarntmonitor/MonitorActivity.java
@@ -0,0 +1,15 @@
+package com.roygalet.www.solarntmonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
+
+        
+    }
+}
